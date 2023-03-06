f0a236b678bf9d3b27ef029363b8e609f66547c8
Chahat Jain <cjain@deqode.com>
2023-03-06 14:59:28 +0530

    Revert "Added all methods"
    This reverts commit 124a709cf1242efd6342df7fd68a64af56ead390.

--- javascript/Array/toString.js
+++
@@ -1,5 +0,0 @@
-const arr = [1, 2, 3, , 4, 4, 5, null, undefined]
-    // arr.join = 1
-
-console.log(arr.toString());
-console.log(arr);
--- javascript/Array/unshift.js
+++
@@ -1,7 +0,0 @@
-const arr = [40, 50, 60, ]
-
-arr.unshift(10, 20)
-
-arr.unshift(30, null, undefined)
-
-console.log(arr);
